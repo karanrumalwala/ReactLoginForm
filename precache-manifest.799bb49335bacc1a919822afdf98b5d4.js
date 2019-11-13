7ef96c31dc47a48451ff88102a266177e9c4b1ae
karanrumalwala <32751689+karanrumalwala@users.noreply.github.com>
2019-11-13 17:09:09 -0500

    Updates

---
+++ precache-manifest.799bb49335bacc1a919822afdf98b5d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab710378f4e27ae1fd0c4fdc5c527346",
+    "url": "/ReactLoginForm/index.html"
+  },
+  {
+    "revision": "cd6cdb9ea628f94441dc",
+    "url": "/ReactLoginForm/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f09327d654927fac4172",
+    "url": "/ReactLoginForm/static/css/main.25f84ef4.chunk.css"
+  },
+  {
+    "revision": "cd6cdb9ea628f94441dc",
+    "url": "/ReactLoginForm/static/js/2.f2b43f99.chunk.js"
+  },
+  {
+    "revision": "f09327d654927fac4172",
+    "url": "/ReactLoginForm/static/js/main.326792fa.chunk.js"
+  },
+  {
+    "revision": "60356e8ad7e6a44ed944",
+    "url": "/ReactLoginForm/static/js/runtime-main.ef882c00.js"
+  },
+  {
+    "revision": "8fe37a85ad7d9dc4bf41ded172f43828",
+    "url": "/ReactLoginForm/static/media/login.8fe37a85.svg"
+  }
+]);
